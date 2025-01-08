aef0d834604ca985571d7bb206237ea69b6b5ddf
Jindřich Bär <jindrichbar@gmail.com>
2025-01-08 13:15:36 +0100

    docs: improve library documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! # retch | browser impersonation made simple
+//! 
+//! Retch is a `rust` library that allows you to impersonate a browser and make requests to websites. It is built on top of `reqwest`, `rustls` and `tokio` and supports HTTP/1.1, HTTP/2, and HTTP/3.
+//! 
+//! 
+//! ### ⚠️ Warning ⚠️
+//!
+//! Technically speaking, the `retch` project is a somewhat thin wrapper around `reqwest` that provides a more ergonomic API for making requests to websites. 
+//! The real strength of `retch` is that it uses patched versions of `rustls` and other libraries that allow it to make browser-like requests.
+//! 
+//! Note that if you want to use this library in your rust project, you have to add the following dependencies to your `Cargo.toml` file:
+//! ```toml
+//! 
+//! [dependencies]
+//! retch = { path="../retch" }
+//! rustls = { version="0.23.16" }
+//! tokio = { version="1.41.1", features = ["full"] }
+//! 
+//! [patch.crates-io]
+//! rustls = { path="../rustls/rustls/" }
+//! reqwest = { path="../reqwest/" }
+//! h2 = { path="../h2/" }
+//! ```
+//! 
+//! 
+//! 
+
 #![deny(unused_crate_dependencies)]
 mod http_headers;
 mod tls;
